6c67242c7e31922b4204bf541dbc8a7f652b57a1
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-14 20:37:46 +0600

    setup daisyUI and theme

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center ">
+      <button className="btn btn-primary">Test Button</button>
+    </div>
+  );
+}
